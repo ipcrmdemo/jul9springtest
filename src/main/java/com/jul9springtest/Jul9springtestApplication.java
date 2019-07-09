1eba6646204301b8596b8263d92e308aa0865973
Matt Cadorette <mcadorette@gmail.com>
2019-07-09 08:52:57 -0400

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/jul9springtest/Jul9springtestApplication.java
@@ -0,0 +1,12 @@
+package com.jul9springtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jul9springtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jul9springtestApplication.class, args);
+	}
+}
